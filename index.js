29c25c7a94cf8fd735bde1bab1d5d46cd49e2fed
Muhammad Aazmeer <aazmeer.official@gmail.com>
2026-02-27 11:01:19 +0500

    404 Page Error Handled

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const port = 3000;
 
 // Express App
 
-app.use((req,res,next)=>{
-    req.time = new Date(Date.now()).toString();
-console.log(req.method,req.path,req.hostname,req.time)
-next()
+// app.use((req,res,next)=>{
+//     req.time = new Date(Date.now()).toString();
+//     console.log(req.method,req.path,req.hostname,req.time)
+//     next()
+// })
+
+app.use("/random",(req,res,next)=>{
+    console.log("I am only for Random")
 })
+
 
 app.get("/",(req,res)=>{
     res.send("Root Page")
@@ -20,6 +25,12 @@ app.get("/",(req,res)=>{
 app.get("/random",(req,res)=>{
     res.send("Random Page")
 }) 
+
+
+// 404 Error
+app.use((req,res)=>{
+    res.send("Page Not Found")
+})
 
 app.listen(port,()=>{ 
     console.log("server is listening...")
